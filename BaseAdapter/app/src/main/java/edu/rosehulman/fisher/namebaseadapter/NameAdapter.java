d87a10ccd0b8d3f4f41d8089156511a054ffe16b
David Fisher <fisherds@rose-hulman.edu>
2018-03-19 15:43:34 -0400

    new project

---
+++ BaseAdapter/app/src/main/java/edu/rosehulman/fisher/namebaseadapter/NameAdapter.java
@@ -0,0 +1,33 @@
+package edu.rosehulman.fisher.namebaseadapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by fisherds on 3/19/18.
+ */
+
+public class NameAdapter extends BaseAdapter {
+  @Override
+  public int getCount() {
+    return 0;
+  }
+
+  // Optional
+  @Override
+  public Object getItem(int i) {
+    return null;
+  }
+
+  // Optional
+  @Override
+  public long getItemId(int i) {
+    return 0;
+  }
+
+  @Override
+  public View getView(int i, View view, ViewGroup viewGroup) {
+    return null;
+  }
+}
