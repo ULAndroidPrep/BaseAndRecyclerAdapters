765bc824ae02926aecd5e585da85ebfc720f2ad0
David Fisher <fisherds@rose-hulman.edu>
2018-03-19 15:58:56 -0400

    layout for a name row

--- BaseAdapter/app/src/main/java/edu/rosehulman/fisher/namebaseadapter/NameAdapter.java
+++ BaseAdapter/app/src/main/java/edu/rosehulman/fisher/namebaseadapter/NameAdapter.java
@@ -1,17 +1,33 @@
 package edu.rosehulman.fisher.namebaseadapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by fisherds on 3/19/18.
  */
 
 public class NameAdapter extends BaseAdapter {
+  private List<String> mNames;
+  private Context mContext;
+
+  public NameAdapter(Context context) {
+    this.mNames = new ArrayList<>();
+    this.mContext = context;
+  }
+
   @Override
   public int getCount() {
-    return 0;
+    return mNames.size();
   }
 
   // Optional
@@ -26,8 +42,20 @@ public class NameAdapter extends BaseAdapter {
     return 0;
   }
 
+
   @Override
-  public View getView(int i, View view, ViewGroup viewGroup) {
-    return null;
+  public View getView(int i, View convertView, ViewGroup parent) {
+    View view = null;
+    if (convertView == null) {
+      view = LayoutInflater.from(mContext).inflate(R.layout.name_view, parent, false);
+    } else {
+      view = convertView;
+    }
+    // TODO: Populate the view with data.
+    TextView nameTextView = view.findViewById(R.id.name);
+    TextView descriptionTextView = view.findViewById(R.id.description);
+
+
+    return view;
   }
 }
